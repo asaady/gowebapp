2a912c289fb7fad335b787f6d5ea06139a690aed
admin <admin@localhost.localdomain>
2020-04-09 10:17:42 +0000

    update+2

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func processFlags() *daemon.Config {
 	cfg := &daemon.Config{}
 
 	flag.StringVar(&cfg.ListenSpec, "listen", "localhost:3000", "HTTP listen spec")
-	flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=/var/run/postgresql dbname=gowebapp sslmode=disable", "DB Connect String")
+	flag.StringVar(&cfg.Db.ConnectString, "db-connect", "host=127.0.0.1 dbname=gowebapp user=postgres password=3@141592Pi sslmode=disable", "DB Connect String")
 	flag.StringVar(&assetsPath, "assets-path", "assets", "Path to assets dir")
 
 	flag.Parse()
